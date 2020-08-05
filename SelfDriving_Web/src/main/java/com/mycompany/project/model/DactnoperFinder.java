41ce76c44ea7114a92f27616c6fdc4b3092dd244
soowoo95 <soowoo95@gmail.com>
2020-08-05 14:20:47 +0900

    0805 2nd commit

---
+++ SelfDriving_Web/src/main/java/com/mycompany/project/model/DactnoperFinder.java
@@ -0,0 +1,22 @@
+package com.mycompany.project.model;
+
+import java.util.Date;
+
+public class DactnoperFinder {
+	
+	private int dperfinder;
+	private String dfinder;
+	public int getDperfinder() {
+		return dperfinder;
+	}
+	public void setDperfinder(int dperfinder) {
+		this.dperfinder = dperfinder;
+	}
+	public String getDfinder() {
+		return dfinder;
+	}
+	public void setDfinder(String dfinder) {
+		this.dfinder = dfinder;
+	}
+	
+}
---
+++ SelfDriving_Web/src/main/java/com/mycompany/project/model/DactnoperTime.java
@@ -0,0 +1,23 @@
+package com.mycompany.project.model;
+
+import java.util.Date;
+
+public class DactnoperTime {
+	
+	private int dactno;
+	private String dpertime;
+	public int getDactno() {
+		return dactno;
+	}
+	public void setDactno(int dactno) {
+		this.dactno = dactno;
+	}
+	public String getDpertime() {
+		return dpertime;
+	}
+	public void setDpertime(String dpertime) {
+		this.dpertime = dpertime;
+	}
+	
+	
+}
